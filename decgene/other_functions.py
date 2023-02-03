7946b4f6c7eee4d0a627a8b6e681e782d42df66a
Robbie Cahill <93015399+robbiecahill10@users.noreply.github.com>
2023-02-03 15:03:05 -0800

    more cleanup

--- decgene/other_functions.py
+++ decgene/other_functions.py
@@ -1,63 +1,5 @@
 """ Other functions for DecGene"""
 
-
-def correlation_map_with_CCF(PPs, original_shape, plot=True,
-                             order_type=1, area_order=None,
-                             put_last_k=0, save_fig=False, save_index=None):
-	''' Compare PPs with the standard ABA CCF.
-	'''
-	# transform PPs to 4d tensor
-	PPs_3d = np.zeros([PPs.shape[0]] + original_shape[1:].tolist())
-	num_pps = PPs.shape[0]
-	for i in range(PPs.shape[0]):
-		p2 = np.reshape(PPs[i, :], original_shape[1:])
-		PPs_3d[i, :, :, :] = p2
-	# load ABA CCF coarse
-	areas_atlas = np.load('../data/mouse_coarse_structure_atlas.npy')
-	mouse_coarse_df = pd.read_pickle('../data/mouse_coarse_df')
-	if area_order != None:
-		cor_mat = np.corrcoef(
-			np.vstack([areas_atlas.reshape(12, -1)[np.array(area_order)], PPs_3d.reshape(num_pps, -1)]))[
-		          :areas_atlas.shape[0], areas_atlas.shape[0]:]
-	else:
-		cor_mat = np.corrcoef(np.vstack([areas_atlas.reshape(12, -1), PPs_3d.reshape(num_pps, -1)]))[
-		          :areas_atlas.shape[0], areas_atlas.shape[0]:]
-
-	if order_type == 1:
-		rows, cols = linear_sum_assignment(-np.abs(cor_mat))
-		factor_order = list(cols) + [x for x in range(num_pps) if x not in cols]
-	elif order_type == 2:
-		cols = np.argmax(np.abs(cor_mat), 0)
-		if put_last_k > 0:
-			# put the poorly fitted patterns at the last.
-			best_fits = [abs(cor_mat[y, x]) for x, y in enumerate(cols)]
-			orders = np.argsort(best_fits)
-			for i in range(put_last_k):
-				cols[orders[i]] = max(cols)
-		factor_order = np.argsort(
-			[10 * x - abs(cor_mat[x, i]) for i, x in enumerate(cols.tolist())])  # first sort by x, then sort by the value
-
-	if plot:
-		fig = plt.gcf()
-		plt.figure(figsize=(10, 9))
-		plt.imshow(np.abs(cor_mat[:, factor_order]).tolist(), cmap='YlOrRd')
-		if area_order is None:
-			plt.yticks(np.arange(12), (mouse_coarse_df.iloc[:]['name'].tolist()))
-		else:
-			plt.yticks(np.arange(12), (mouse_coarse_df.iloc[area_order]['name'].tolist()))
-		plt.ylim([-0.5, 11.5])
-		plt.gca().invert_yaxis()
-		plt.xticks(range(num_pps), factor_order)
-		plt.title('Correlation Coefficient')
-		plt.xlabel('Principle Patterns')
-		plt.colorbar(shrink=.6)
-		plt.savefig('../figures/corr_coeff.png')
-		plotname = "corr_map.png"
-		plt.savefig(plotname, dpi=300)
-		plt.show()
-	return np.abs(cor_mat[:, factor_order])
-
-
 def geneReconstructionAcccuracy(_PPs, _coeffs, _filtered_data):
 	""" function create gene-by-gene reconstruction accuracy """
 
@@ -89,7 +31,10 @@ def geneReconstructionAcccuracy(_PPs, _coeffs, _filtered_data):
 
 def show_graph_with_labels(adjacency_matrix, mylabels, colors, title=""):
 	""" Function to generate graphs of correlation networks """
-	# plt.figure(figsize=(30,30))
+
+	import matplotlib.pyplot as plt
+	import numpy as np
+
 	fig1, f1_axes = plt.subplots(ncols=2, nrows=2, constrained_layout=True, figsize=(25, 14))
 	rows, cols = np.where(adjacency_matrix > 0)
 	edges = [(x, y) for x, y in zip(rows.tolist(), cols.tolist()) if x > y]
@@ -128,12 +73,11 @@ def show_graph_with_labels(adjacency_matrix, mylabels, colors, title=""):
 	sm._A = []
 	plt.colorbar(sm)
 	plt.title(title)
-	plt.savefig("../figures/network_names{}.png".format(title), dpi=300)
 	plt.show()
 
 
 def visualize_gene(geneA, template=0, colorA='green', missing_mask=True):
-	""" define_function a function that shows two genes together """
+	""" define a function that shows two genes together """
 
 	import numpy as np
 	from PIL import Image, ImageFilter
@@ -169,9 +113,7 @@ def visualize_gene(geneA, template=0, colorA='green', missing_mask=True):
 
 	for rgb_ch in range(3):
 		out1[:, :, rgb_ch] = out1[:, :, rgb_ch] + support_x
-	#         out1[:,:,1] = support_x
 	c.imshow(out1)
-	# plt.colorbar()
 
 	out2 = np.zeros((x_dim, z_dim, 3))
 	out2[:, :, colorA] = np.sum(geneA, 1)
@@ -190,9 +132,7 @@ def visualize_gene(geneA, template=0, colorA='green', missing_mask=True):
 
 	for rgb_ch in range(3):
 		out2[:, :, rgb_ch] = out2[:, :, rgb_ch] + support_y
-	#         out2[:,:,1] = support_y
 	b.imshow(out2)
-	# plt.colorbar()
 
 	plt.figure()
 
@@ -213,13 +153,8 @@ def visualize_gene(geneA, template=0, colorA='green', missing_mask=True):
 
 	for rgb_ch in range(3):
 		out3[:, :, rgb_ch] = out3[:, :, rgb_ch] + support_z.T
-	#         out3[:,:,1] = support_z.T
 
 	a.imshow(out3)
-
-	#     a.text(1, 6, 'PP{}'.format(i), color='w', fontsize=30)
-	#     b.text(1, 6, 'PP{}'.format(i), color='w', fontsize=30)
-	#     c.text(1, 6, 'PP{}'.format(i), color='w', fontsize=30)
 
 	a.axes.get_xaxis().set_visible(False)
 	a.axes.get_yaxis().set_visible(False)
@@ -246,7 +181,7 @@ def weighted_correlation(A, weights, demean=True):
 
     if demean:
         A = A - np.mean(A, 0, keepdims=True)
-    mean_A = 0#np.sum(A * weights, 1, keepdims=True) / np.sum(weights)
+    mean_A = 0
     cov = (A - mean_A) @ (A - mean_A).T
     corr = np.diag(np.diag(cov) ** -.5) @ cov @ np.diag(np.diag(cov)**-.5)
     return corr
